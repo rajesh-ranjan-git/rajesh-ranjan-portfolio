738648cb87d4840cb31a4f71beab0363a4867a2f
Rajesh <rajeshranjan8271@gmail.com>
2024-08-13 10:35:19 +0530

    Changed Title

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <h1 className="p-10 text-5xl font-semibold text-blue-900">
+        <h1 className="p-10 text-5xl font-semibold text-blue-900 underline">
           This is my Portfolio site - work in progress...
         </h1>
       </div>

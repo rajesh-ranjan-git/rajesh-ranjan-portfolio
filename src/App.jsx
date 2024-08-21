593a6b2f554700117c986a01d748b4c915955a71
Rajesh <rajeshranjan8271@gmail.com>
2024-08-21 14:21:44 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="p-10 text-5xl font-semibold text-blue-900 underline bg-amber-500 rounded-2xl">
-          This is my Portfolio site - work in progress...
-        </h1>
-      </div>
+      <Navbar />
+      <Outlet />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,22 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home";
 
-createRoot(document.getElementById("root")).render(<App />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter} />
+);

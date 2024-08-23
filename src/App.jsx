54c3584b8cbf9d4f09a8aef34aa4eca0b5e32f29
Rajesh <rajeshranjan8271@gmail.com>
2024-08-23 08:47:28 +0530

    Title handled dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import favicon1 from "../public/title_pic-circular.png";
+import favicon2 from "../public/namaste.png";
+import "./App.scss";
+
+const handleTitle = () => {
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == "hidden") {
+      document.title = "Come back to portfolio";
+      document.getElementsByTagName("link")[0].href = favicon2;
+    } else {
+      document.title = "Rajesh Ranjan | Software Developer Portfolio";
+      document.getElementsByTagName("link")[0].href = favicon1;
+    }
+  });
+};
 
 function App() {
-  return <></>;
+  handleTitle();
+  return <>Rajesh Ranjan | Portfolio</>;
 }
 
 export default App;

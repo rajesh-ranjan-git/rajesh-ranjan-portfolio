0ffb576e95c8e17e3385f9698b5f2802fe8b88ec
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-09-02 21:18:19 +0530

    Code not deploying

--- src/containers/portfolio/Lightbox/Slides.jsx
+++
@@ -1,8 +0,0 @@
-import { projectsHeader } from "../../../Components/Portfolio/Portfolio";
-
-export const advancedSlides = projectsHeader.projects.map((project, key) => ({
-  src: project.image,
-  title: project.title,
-  description: project.subtitle,
-  filter: project.filter,
-}));

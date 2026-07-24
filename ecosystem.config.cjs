73c167e098402b447194ce8256ae86075a140cbb
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-07-24 18:14:45 +0530

    deployment working, slight update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
       script: "npm",
       args: "start",
 
-      instances: 1,
-      exec_mode: "fork",
+      instances: 2,
+      exec_mode: "cluster",
 
       env: {
         NODE_ENV: "production",

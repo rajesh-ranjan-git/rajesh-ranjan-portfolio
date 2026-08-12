17d1c3dfeef797c361512fc83854efcd24339f81
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-08-12 23:43:24 +0530

    2.0.1

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
       script: "npm",
       args: "start",
 
-      instances: 2,
-      exec_mode: "cluster",
+      instances: 1,
+      exec_mode: "fork",
 
       env: {
         NODE_ENV: "production",
